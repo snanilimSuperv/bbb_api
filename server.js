89554ddfa4adfffd7c6a457ddcdba06074a03562
aiaoio <snanilim@gmail.com>
2018-01-24 18:18:30 +0600

    init setup processing

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-var express = require('express');
+// ---------------------------------- Requirement Load Start ---------------------------------- // 
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan')
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
-var app = express();
-var server = app.listen(4000, function(){
-    console.log('App Start');
+// ---------------------------------- Requirement Load End ---------------------------------- // 
+// Load environment variables from .env file
+dotenv.load();
+
+const app = express();
+mongoose.connect(process.env.MongoDB);
+mongoose.connection.on('error', function() {
+    console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+    process.exit(1);
+});
+
+
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.listen(app.get('port'), function(){
+    console.log('App Start on ' + app.get('port'));
 })
+
