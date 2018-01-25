77c4813d47228284d4f190aee38e752a27a8e70c
aiaoio <snanilim@gmail.com>
2018-01-25 17:50:42 +0600

    added some req

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // ---------------------------------- Requirement Load Start ---------------------------------- // 
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan')
+const logger = require('morgan');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const request = require('request');
 const dotenv = require('dotenv');
 
 // ---------------------------------- Requirement Load End ---------------------------------- // 
@@ -19,9 +23,18 @@ mongoose.connection.on('error', function() {
 
 
 app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(cookieParser());
 
 
 app.listen(app.get('port'), function(){
     console.log('App Start on ' + app.get('port'));
 })
+
+
+
 
