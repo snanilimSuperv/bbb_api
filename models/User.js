61f8eef7fb2be727e85f5238accb121991c38859
aiaoio <snanilim@gmail.com>
2018-01-31 00:08:19 +0600

    added salt hash

--- models/User.js
+++ models/User.js
@@ -28,6 +28,41 @@ const userSchema = new mongoose.Schema({
 }, schemaOptions);
 
 
+userSchema.pre('save', function(next){
+    var user = this;
+    if (!user.isModified('password')) { return next(); }
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            user.password = hash;
+            next();
+        });
+    });
+
+})
+
+userSchema.methods.comparePassword = function(password, cb) {
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        cb(err, isMatch);
+    });
+};
+
+userSchema.virtual('gravatar').get(function() {
+    if (!this.get('email')) {
+        return 'https://gravatar.com/avatar/?s=200&d=retro';
+    }
+    var md5 = crypto.createHash('md5').update(this.get('email')).digest('hex');
+    return 'https://gravatar.com/avatar/' + md5 + '?s=200&d=retro';
+    });
+
+    userSchema.options.toJSON = {
+    transform: function(doc, ret, options) {
+        delete ret.password;
+        delete ret.passwordResetToken;
+        delete ret.passwordResetExpires;
+    }
+};
+
+
 var User = mongoose.model('User', userSchema);
 
 module.exports = User;
